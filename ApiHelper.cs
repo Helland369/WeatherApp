75e1a00c15de7acfe6f843962919a236fe67eea5
Thomas Helland <thomas_helland@pm.me>
2025-01-24 11:16:01 +0100

    Trying to add ipapi

--- ApiHelper.cs
+++ ApiHelper.cs
@@ -2,18 +2,22 @@ namespace WeatherApp;
 
 public class ApiHelper
 {
-    private string _apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Fredrikstad&appid=//APIKEY";
-
     private HttpClient _httpClient;
 
     public ApiHelper()
     {
         _httpClient = new HttpClient();
-        _httpClient.BaseAddress = new Uri(_apiUrl);
     }
 
     public string GettWeatherData()
     {
-        return _httpClient.GetStringAsync(_apiUrl).GetAwaiter().GetResult();
+        string weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Fredrikstad&appid=//APIKEY&units=metri";
+        return _httpClient.GetStringAsync(weatherApiUrl).GetAwaiter().GetResult();
+    }
+
+    public string GetPositionData()
+    {
+        string postionApiUrl = "https://ipapi.co/json";
+        return _httpClient.GetStringAsync(postionApiUrl).GetAwaiter().GetResult();
     }
 }
--- Program.cs
+++ Program.cs
@@ -10,12 +10,16 @@ class Program
     {
         ApiHelper api = new ApiHelper();
 
+        string positonData = api.GetPositionData();
         string json = api.GettWeatherData();
         
+        LocationData ld = JsonSerializer.Deserialize<LocationData>(positonData);
         WeatherData wd = JsonSerializer.Deserialize<WeatherData>(json);
         
         Console.WriteLine($"Temp: {wd.main.temp}");
         Console.WriteLine($"Humidity: {wd.main.humidity}");
         Console.WriteLine($"Pressure: {wd.main.pressure}");
+        Console.WriteLine($"City {ld.city}");
+        Console.WriteLine($"Country {ld.country}");
     }
 }
