1a9abac00a0d0e86051aceab0e14e1c1f98dd9e6
Thomas Helland <thomas_helland@pm.me>
2025-01-26 16:33:48 +0100

    edit api key

--- obj/Debug/net9.0/WeatherApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/WeatherApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b93ecf30dbc436f7330d64f9044b9f90ec6850b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697b33c1b29a9e8de7774f68957b10c91a9db700")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
