011b682edc8eba72ff077b23bacfafc630ea17d7
Thomas Helland <thomas_helland@pm.me>
2025-02-17 11:07:47 +0100

    Added router

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,10 +1,9 @@
 <script setup>
-import nav_bar from './components/nav_bar.vue'
-import current_weather from './components//current_weather.vue'
-//import data_base_weather from './components/data_base_weather.vue'
+import nav_bar from '@/components/nav_bar.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
   <nav_bar />
-  <current_weather />
+  <RouterView />
 </template>
