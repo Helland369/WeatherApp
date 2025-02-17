222ac5536f3b1b582adc081d4bfcccf2d2286e46
Thomas Helland <thomas.helland97@gmail.com>
2025-02-17 14:44:09 +0100

    There is bugs...

--- Frontend/src/components/current_weather.vue
+++ Frontend/src/components/current_weather.vue
@@ -25,29 +25,17 @@ export default {
 </script>
 
 <template>
-  <div>
-    <table v-if="Object.keys(weatherData).length" class="w-full border-collaps mt-2">
-      <thead>
-        <tr>
-          <th
-            v-for="(value, key) in weatherData[0]"
-            :key="key"
-            class="border border-gray-300 bg-gray-900 text-left p-2"
-          >
-            {{ key }}
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td v-for="(value, key) in weatherData" :key="key" class="border border-gray-300 p-2">
-            {{ value }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <p v-else>No data available</p>
+  <div v-if="weatherData">
+    <h2>Current weather for your location</h2>
+
+    <p><strong>Location:</strong> {{ weatherData.name }}</p>
+    <!-- <p><strong>Temperature:</strong> {{ weatherData.main.temp }}</p> -->
+    <!-- <p><strong>Minimum Temperature:</strong> {{ weatherData.main.temp_min }}</p> -->
+    <!-- <p><strong>Maximum Temperature:</strong> {{ weatherData.main.temp_max }}</p> -->
+    <!-- <p><strong>Feels like:</strong> {{ weatherData.main.feels_like }}</p> -->
+    <!-- <p><strong>Humidity:</strong> {{ weatherData.main.humidity }}</p> -->
+    <!-- <p><strong>Pressure:</strong> {{ weatherData.main.pressure }}</p> -->
+    <!-- <p><strong>Sea level:</strong> {{ weatherData.main.sea_level }}</p> -->
+    <!-- <p><strong>Ground level:</strong> {{ weatherData.main.grnd_level }}</p> -->
   </div>
 </template>
-
-<style></style>
