c18f9f93dbdf26c5f3da85a39649e44330781d6c
Thomas Helland <thomas.helland97@gmail.com>
2025-02-17 11:07:47 +0100

    Added router

---
+++ Frontend/src/views/homeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import current_weather from '@/components//current_weather.vue'
+import data_base_weather from '@/components/data_base_weather.vue'
+</script>
+
+<template>
+  <current_weather />
+  <current_weather />
+</template>
